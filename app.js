db4945f7a5a5b108969d25142bc95f0242e6cb6e
Евгения Сыропятова <e.syropyatova@altuera.com>
2021-08-02 09:17:34 +0300

    build

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const dotenv = require('dotenv');
 
 app.use(express.static(path.resolve('../frontend/build/')));
 
@@ -8,4 +9,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve('../frontend/build/index.html'));
 });
 
-app.listen(3000);
+app.listen(process.env.port || 3000);
